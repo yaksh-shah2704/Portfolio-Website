d4877ee610c330f65f4a067c2c020ebcd9460dd3
yaksh-shah2704 <yakshshah2704@gmail.com>
2025-05-25 15:00:17 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+const typed = new Typed('.multiple-text', {
+      strings: ['Frontend Developer', 'Backend Developer', 'Full-Stack Developer', 'AI/ML Developer', 'Web Designer'],
+      typeSpeed: 50,
+      backSpeed: 80,
+      backDelay: 1200,
+      loop: true,
+    });
+
+// const typed = new Typed('.multiple-text', {
+//     strings: ['Frontend Developer', 'Backend Developer', 'Full-Stack Developer', 'AI/ML Developer', 'Web Designer'],
+//     typeSpeed: 80,
+//     backSpeed: 80,
+//     backDelay: 1200,
+//     loop: true,
+// });
